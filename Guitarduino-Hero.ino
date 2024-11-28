b3b34534470d0b58a0819dc3a18574c8eb17ce9a
Nirv.Ana <anmartan@ucm.es>
2024-11-28 15:21:54 +0100

    ADDED: Pitches.h
    Added a file with the notes that will be used for the songs

---
+++ Guitarduino-Hero.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}c:\Users\dalem\Desktop\Master\Didactica II\guitarhero\Guitarduino-Hero\songs\pitches.h
